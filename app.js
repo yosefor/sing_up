93bcf11d690eff17cbf38392f6fba69590bb42f6
Yosef Schwartz <yossfch749@gmail.com>
2022-05-27 19:04:35 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const experss = require('express');
+const bodyParser = require('body-parser');
+const https = require('https');
+
+const app = experss();
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(experss.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/singup.html');
+});
+
+app.post('/failuer', (req, res) => {
+    res.redirect('/');
+});
+
+
+app.post('/', (req, res) => {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    // console.log(firstName);
+    // console.log(lastName);
+    // console.log(email);
+    // res.send('Thank you!');
+
+    const url = 'https://us18.api.mailchimp.com/3.0/lists/8ef6d3972a';
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const options = {
+        method: 'POST',
+        auth: 'yosef:6475361ce9342a35ce3fc112e77ba7a8-us18',
+    };
+
+    const request = https.request(url, options ,(response) => {
+        // console.log(response.statusCode);
+        // console.log(response.statusMessage);
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html');
+        }
+        else {
+            res.sendFile(__dirname + '/failure.html');
+        }
+        response.on('data', (data) => {
+            // console.log(JSON.parse(data));
+        });
+    });
+    request.write(jsonData);
+    request.end();
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server started on port 3000');
+});
